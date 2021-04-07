4cbf94dc2bcd0f5ec52633bb237bfe751ea5908b
Abdullloh <abdullohjonrgashev@gmail.com>
2021-04-07 13:33:08 +0500

    my portfolio has been updated

--- main.js
+++ main.js
@@ -9,6 +9,32 @@ var wow = new WOW({
 		// the argument that is passed in is the DOM node being animated
 	},
 	scrollContainer: null, // optional scroll container selector, otherwise use window,
-	resetAnimation: false // reset animation on end (default is true)
+	resetAnimation: true // reset animation on end (default is true)
 });
 wow.init();
+$('document').ready(function(){
+	var typed = new Typed('.animate', {
+		strings:["I'm Abdulloh Irgashev"," Front End Developer" ],
+		typeSpeed: 50,
+		backSpeed:50,
+		showCursor:false,
+		loop:true
+	  });
+});
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
